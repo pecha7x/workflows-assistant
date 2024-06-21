ec20f28e7c3cdedd5bfff6836afcc07e2213e8dc
Artyom Pechyorin <pecha7x@gmail.com>
2024-06-21 22:03:55 +0400

    fix: New production IP address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 set :rails_env, 'production'
 set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-server '2.56.206.168', user: 'deploy', roles: %w[web app db nginx], port: 2257
+server '2.56.205.189', user: 'deploy', roles: %w[web app db nginx], port: 2257
