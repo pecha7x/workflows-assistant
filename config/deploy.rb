952cf13d5a5de10efc1b7bcd611cf0d6ab08228f
Artyom Pechyorin <pecha7x@gmail.com>
2024-06-22 23:14:55 +0400

    fix: Include production credentails to deploy process

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:pecha7x/workflows-assistant.git'
 set :branch, :main # use `git rev-parse --abbrev-ref HEAD`.chomp for pick current branch
 set :deploy_to, '/home/deploy/workflows-assistant'
 set :pty, true
-set :linked_files, %w[config/database.yml config/master.key config/credentials/production.key config/credentials/production.yml.enc]
+set :linked_files, %w[config/database.yml config/master.key config/credentials/production.key]
 set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads]
 set :keep_releases, 3
 set :nvm_node, 'v18.7.0'
